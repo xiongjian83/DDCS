cc06d32b43ad2e9152d52558b996ce6165f41b0d
black-06 <hello.bug@foxmail.com>
2025-03-07 10:00:27 +0800

    ci: add github action release script

--- ddcs.py
+++ ddcs.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2024 ASXE  All Rights Reserved 
+# Copyright (C) 2024 ASXE  All Rights Reserved
 #
 # @Time    : 2024/8/9 下午4:17
 # @Author  : ASXE
@@ -24,46 +24,58 @@ def cost_time(func):
 
 
 @cost_time
-def run(root_path, config_path):
-    log.info('脚本已启动...')
+def run(root_path: str, config_path: str, process_asar: bool):
+    log.info("脚本已启动...")
     time.sleep(1)
 
-    DDProcessor(True)
+    if process_asar:
+        DDProcessor(True)
 
     fp = FileProcessor(root_path, config_path)
     file_paths = fp.recursive_listdir()
-    log.info('汉化开始')
+    log.info("汉化开始")
     for transformation in fp.get_transformations():
-        search = transformation['src']
-        replacement = transformation['dest']
+        search = transformation["src"]
+        replacement = transformation["dest"]
         replaced = fp.process_files(file_paths, search, replacement)
         if not replaced:
             log.warn(search)
         else:
             log.info(f"{search} -> {replacement}")
 
-    DDProcessor(False)
+    if process_asar:
+        DDProcessor(False)
 
 
 @cost_time
-def run_v2(root_path: str):
+def run_v2(root_path: str, process_asar: bool):
     log.info("脚本已启动...")
     time.sleep(1)
 
-    DDProcessor(True)
+    if process_asar:
+        DDProcessor(True)
     log.info("汉化开始")
     replace(Path(root_path))
-    DDProcessor(False)
+    if process_asar:
+        DDProcessor(False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--v2", action="store_true")
+    parser.add_argument("--root_path", type=str, default=None)
     args = parser.parse_args()
 
-    root_path = "./app/build/"
+    if not args.root_path:
+        # 没有传递 root_path, 认为需要从安装目录 cp
+        root_path = "./app/build/"
+        process_asar = True
+    else:
+        root_path = args.root_path
+        process_asar = False
+    print(root_path, process_asar)
     config_path = "./config.json"
     if args.v2:
-        run_v2(root_path)
+        run_v2(root_path, process_asar)
     else:
-        run(root_path, config_path)
+        run(root_path, config_path, process_asar)
